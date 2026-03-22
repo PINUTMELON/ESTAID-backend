1e80ff3a909767135adad9f69ad2f43dc66560d6
ioi34 <nmcminyu@naver.com>
2026-03-22 21:48:19 +0900

    fix: 프로젝트 생성시 backgroundImageUrl 요구 제거, 프로젝트 수정 전용과 생성 전용 분리

---
+++ src/main/java/com/estaid/project/dto/ProjectCreateRequest.java
@@ -0,0 +1,15 @@
+package com.estaid.project.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ProjectCreateRequest {
+
+    @NotBlank(message = "프로젝트 제목을 입력해주세요.")
+    @Size(max = 200, message = "프로젝트 제목은 200자 이하로 입력해주세요.")
+    private String title;
+}
